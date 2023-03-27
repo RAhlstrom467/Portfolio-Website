900aa560890d0a16dc86ca4d39a6de43499a0502
Ryan_Ahlstrom <Ryan.Ahlstrom@gmail.com>
2023-03-27 09:48:34 -0600

    Added some card detail

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -16,36 +16,58 @@ function App() {
         GitHub Projects and Links, use cards
       </h2>
       <div className="cards">
+      <div className='c1'>
       <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Project One</Card.Title>
+        <Card.Title>Student Registration</Card.Title>
         <Card.Text>
-          Bio of the project
+          Full Stack Website utilizing React & SpringBoot Frameworks
+          Backend written in Java, Frontend written in JavaScript
         </Card.Text>
         <Button variant="primary">GitHub Link</Button>
       </Card.Body>
       </Card>
+      </div>
+      <div className='c2'>
       <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Project One</Card.Title>
+        <Card.Title>Movie Theater App</Card.Title>
         <Card.Text>
-          Bio of the project
+          Full Stack Website utilizing React & Springboot Frameworks
+          Backend written in Java, Frontend written in JavaScript
         </Card.Text>
         <Button variant="primary">GitHub Link</Button>
       </Card.Body>
       </Card>
+      </div>
+      <div className='c3'>
       <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Project One</Card.Title>
+        <Card.Title>Find My Mentor</Card.Title>
         <Card.Text>
-          Bio of the project
+        Full Stack Website utilizing React & Express
+        Backend written in Node.js, Frontend written in JavaScript
         </Card.Text>
         <Button variant="primary">GitHub Link</Button>
       </Card.Body>
       </Card>
+      </div>
+      <div className='c3'>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Digital Twin Capstone Project</Card.Title>
+        <Card.Text>
+        Full Stack Website utilizing React & Express
+        Backend written in Node.js, Frontend written in JavaScript
+        </Card.Text>
+        <Button variant="primary">GitHub Link</Button>
+      </Card.Body>
+      </Card>
+      </div>
       </div>
     </div>
   );
